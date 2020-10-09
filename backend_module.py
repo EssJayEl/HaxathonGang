60ed8615ef62cfaad7176a5f9de8a4a5e69c809c
EssJayEl <38003796+EssJayEl@users.noreply.github.com>
2020-10-09 19:16:44 +0530

    Create backend_module.py

---
+++ backend_module.py
@@ -0,0 +1,8 @@
+def columns(age):
+    #pull columns based on age
+
+def salaryFormat(salary, columnList):
+    #format outputs based on salary and percentages
+
+def sliderCalculation(columnNumber, sliderPercentage, salary, columnList):
+    #adjust other columns based on given columnNumber sliderPercentage
