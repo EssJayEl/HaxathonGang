305c01f43925d79fc58d77fbfa0a53733c061ec7
SaiRuthvik12 <72613093+SaiRuthvik12@users.noreply.github.com>
2020-10-10 09:07:27 +0530

    FILE COMPLETE, SAMPLE VALUES

--- textConverter.py
+++ textConverter.py
@@ -1,5 +1,32 @@
 def pull():
     #pulls data from textfile
+    f= open("data1.txt",'r')
+    keys = ["grp", "fd", "equity","loan","leisure"]
+    l = f.readlines()
+    ret = []
+    for i in l:
+        ret.append(dict())
+        #i.rstrip()
+        i=i[:-1]
+        split = i.split(":")
+        #split[-1].rstrip("\n")
+        #print(split)
+        for j in range(0, len(keys)):
+                
+                ret[-1][keys[j]] = split[j]
+    #print(ret)
+    
+    return ret
+            
 
 def column(n):
     #returns dictionary containing required age group
+    ret = pull()
+    d = {}
+    for i in ret:
+        if(i['grp']==n):
+            d=i;
+    print(d)
+
+#f= open("data1",'r')
+#pull()
